d35c6c6cd788b10b4a4306696ac185451dc91960
SamarBenRhouma <100835595+SamarBenRhouma@users.noreply.github.com>
2022-03-16 20:49:22 +0100

    a

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -54,13 +54,7 @@
       <nav id="navbar" class="navbar">
         <ul>
 <<<<<<< HEAD:resources/views/Layout.blade.php
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+<li><a class="getstarted scrollto" href="http://127.0.0.1:8000/Login">connexion</a></li>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
