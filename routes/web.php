f1f347ac03edca3c6497d21ef08287e0b50cc560
zahramarrrr <101492416+zahramarrrr@users.noreply.github.com>
2022-03-17 09:25:56 +0100

    now

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/Login', function () {
+    return view('Login');
 });
 
 Route::get('/dashboard', function () {
