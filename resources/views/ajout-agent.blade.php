9a8164fa60ec14bf39b09866fa2778037574bad7
zahramarrrr <101492416+zahramarrrr@users.noreply.github.com>
2022-04-08 15:19:58 +0100

    register

--- resources/views/ajout-agent.blade.php
+++ resources/views/ajout-agent.blade.php
@@ -265,7 +265,10 @@
 
             <!-- Recent Sales -->
             
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
+<form method="POST" action="{{ route('register') }}">
+    @csrf
          
 		<div class="container">
 			<div class="row justify-content-center">
@@ -283,7 +286,7 @@
 									<p class="mb-4">Entrez les informations nécessaires:</p>
 								
 									
-									<form method="POST" id="contactForm" name="contactForm" class="contactForm">
+								<!-- 	<form method="POST" id="contactForm" name="contactForm" class="contactForm">
 										<div class="row">
 											<div class="col-md-12">
 												<div class="form-group">
@@ -295,30 +298,42 @@
 											</div>
 											<div class="col-md-12"> 
 												
-											</div>
-											<div class="col-md-12">
-												<div class="form-group">
-													<input type="text" class="form-control" name="subject" id="subject" placeholder="Adresse mail">
-												</div>
-                       
-											</div>
-                                            <div class="col-md-12">
-												<div class="form-group">
-													<input type="text" class="form-control" name="subject" id="subject" placeholder="Télèphone">
-												</div>
-											</div>
-											<div class="form-group">
-													<input type="text" class="form-control" name="subject" id="subject" placeholder="ID Agent d'entrepot">
-												</div>
-                        <div class="form-group">
-													<input type="text" class="form-control" name="subject" id="subject" placeholder="Mot de passe">
-</div>
-											<div class="col-md-12">
-												<div class="form-group">
-													<input type="submit" value="Ajouter" class="btn btn-primary">
-													<div class="submitting"></div>
-												</div>
-											</div>
+											</div> -->
+											<div>
+                <x-label for="name" :value="__('Name')" />
+
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+            
+                      <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+											<div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+            </div>
+                        <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required />
+            </div>
+										
+            <div class="flex items-center justify-end mt-4">
+                
+
+                <x-button class="ml-4">
+                    {{ __('Register') }}
+                </x-button>
+            </div>
 										</div>
 									</form>
 									
