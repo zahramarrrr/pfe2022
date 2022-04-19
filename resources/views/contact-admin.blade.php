c325569381ae47339d93dfa5012d45b64ccff3a0
zahramarrrr <101492416+zahramarrrr@users.noreply.github.com>
2022-04-19 13:40:06 +0100

    listesperso

---
+++ resources/views/contact-admin.blade.php
@@ -0,0 +1,48 @@
+@extends("layouts.commerçant")
+ @section("content")
+<main id="main" class="main">
+<div class="container-contact100">
+				<div class="contact100-pic js-tilt" data-tilt>
+					<img src="assets/contact/images/img-01.png" alt="IMG">
+				</div>
+
+				<form class="contact100-form validate-form">
+					<span class="contact100-form-title">
+					Comment nous pouvons vous aider?
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Name is required">
+						<input class="input100" type="text" name="name" placeholder="Nom et prénom">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Adresse e-mail">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Message is required">
+						<textarea class="input100" name="message" placeholder="Message"></textarea>
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-contact100-form-btn">
+						<button class="contact100-form-btn">
+							Envoyer
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+    @endsection
+
+
+
+	
+
